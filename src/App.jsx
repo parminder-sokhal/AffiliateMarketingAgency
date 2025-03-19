ffb8d8b11c8650507070cd7e7c6a0fe2778664cf
PARMINDER SOKHAL <parmindersokhal22@gmail.com>
2025-03-19 16:15:04 +0530

    loctionerror

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,54 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HeaderDefault from './components/HeaderDefault';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import HomePage from "./pages/Home/HomePage"
-import AboutUs from './pages/AboutUs/AboutUs';
-import Services from './pages/Services/Services';
-import LetsDoWorkTogether from './pages/Home/LetsDoWorkTogether';
-import Privacypolicy from './pages/Home/privacypolicy';
-import Disclaimer from './pages/Home/Disclaimer';
-import TermCondition from './pages/Home/TermCondition';
-import ContactUs from './pages/ContactUs/ContactUs';
-import "./app.css"
-
-
-
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
+import HeaderDefault from "./components/HeaderDefault";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomePage from "./pages/Home/HomePage";
+import AboutUs from "./pages/AboutUs/AboutUs";
+import Services from "./pages/Services/Services";
+import LetsDoWorkTogether from "./pages/Home/LetsDoWorkTogether";
+import Privacypolicy from "./pages/Home/privacypolicy";
+import Disclaimer from "./pages/Home/Disclaimer";
+import TermCondition from "./pages/Home/TermCondition";
+import ContactUs from "./pages/ContactUs/ContactUs";
+import "./app.css";
 
 function App() {
-  const isHomePage = location.pathname === "/";
+  const location = useLocation(); // Get current location from the router
 
   return (
     <>
-    <Router>
-      {isHomePage ? <Header /> : <HeaderDefault/>}
+      {/* Conditionally render Header or HeaderDefault based on the current path */}
+      {location.pathname === "/" ? <Header /> : <HeaderDefault />}
+
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/aboutus" element={<AboutUs/>} />
-        <Route path="/services" element={<Services/>} />
-        <Route path="/privacypolicy" element={<Privacypolicy/>} />
-        <Route path="/Disclaimer" element={<Disclaimer/>} />
-        <Route path="/TermCondition" element={<TermCondition/>} />
-        <Route path="/ContactUs" element={<ContactUs/>} />
+        <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/privacypolicy" element={<Privacypolicy />} />
+        <Route path="/Disclaimer" element={<Disclaimer />} />
+        <Route path="/TermCondition" element={<TermCondition />} />
+        <Route path="/ContactUs" element={<ContactUs />} />
         {/* <Route path="/contact" element={<ContactPage />} /> */}
       </Routes>
-      <LetsDoWorkTogether/>
-      <Footer/>
-    </Router>
+
+      <LetsDoWorkTogether />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+function AppWithRouter() {
+  return (
+    <Router>
+      <App />
+    </Router>
+  );
+}
+
+export default AppWithRouter;
