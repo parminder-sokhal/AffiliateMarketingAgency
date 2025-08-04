8cd64d1716295d71860d80cb7a3466ee458c4a57
PARMINDER SOKHAL <parmindersokhal22@gmail.com>
2025-08-04 12:02:04 +0530

    ok

--- src/pages/ContactUs/GetInTouch.jsx
+++ src/pages/ContactUs/GetInTouch.jsx
@@ -25,63 +25,28 @@ const GetInTouch = () => {
 
   return (
     <div className="get-in-touch-container">
-      <div className="contact-info">
-        <h1>Contact Us</h1>
-        <p>
-          <PiMapPinFill />
-          &nbsp;Phase 5 Sector 59, Sahibzada Ajit Singh Nagar, Punjab
-        </p>
-        <p>
-          <IoMail />
-          &nbsp;info@linkpro.com
-        </p>
-        <p>
-          <FaPhoneAlt />
-          &nbsp;+91 8409120206
-        </p>
-        <p>
-          <MdOutlineAccessTime />
-          &nbsp;Monday - Friday 9:00 AM - 10:00 PM
-        </p>
-      </div>
-      <div className="form-container">
-        <h1>Get In Touch</h1>
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            name="name"
-            placeholder="Enter Name"
-            value={formData.name}
-            onChange={handleChange}
-            required
-          />
-          <input
-            type="email"
-            name="email"
-            placeholder="Enter Email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-          <input
-            type="text"
-            name="phone"
-            placeholder="Enter Phone"
-            value={formData.phone}
-            onChange={handleChange}
-            required
-          />
-          <textarea
-            name="message"
-            placeholder="Your Message"
-            value={formData.message}
-            onChange={handleChange}
-            required
-          ></textarea>
-          <button type="submit">Send Message</button>
-        </form>
-      </div>
+  <div className="form-wrapper">
+    <div className="contact-info">
+      <h1>Contact Us</h1>
+      <p><PiMapPinFill size={30} />&nbsp;Phase 5 Sector 59, Sahibzada Ajit Singh Nagar, Punjab</p>
+      <p><IoMail size={20}/>&nbsp;info@linkpro.com</p>
+      <p><FaPhoneAlt size={20}/>&nbsp;+91 8409120206</p>
+      <p><MdOutlineAccessTime size={25}/>&nbsp;Mon - Fri 9:00 AM - 10:00 PM</p>
     </div>
+
+    <div className="form-container">
+      <h1>Get In Touch</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" placeholder="Enter Name" value={formData.name} onChange={handleChange} required />
+        <input type="email" name="email" placeholder="Enter Email" value={formData.email} onChange={handleChange} required />
+        <input type="text" name="phone" placeholder="Enter Phone" value={formData.phone} onChange={handleChange} required />
+        <textarea name="message" placeholder="Your Message" value={formData.message} onChange={handleChange} required></textarea>
+        <button type="submit">Send Message</button>
+      </form>
+    </div>
+  </div>
+</div>
+
   );
 };
 
