649357c88a3e7df2a4ed7e7c5185aca35182dea2
PARMINDER SOKHAL <parmindersokhal22@gmail.com>
2025-08-13 13:04:25 +0530

    fix case sensitivity

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -1,26 +1,24 @@
-import React from 'react'
-import DefaultBanner from '../../components/DefaultBanner'
-import AboutusLinkPro from './AboutusLinkPro'
-import AboutusWhyChoose from './AboutusWhyChoose'
-import AboutUsWhatWeDo from './AboutUsWhatWeDo'
-import AboutOurValues from './AboutOurValues'
-import AboutTestimonials from './AboutTestimonials'
-import AboutMeetOurTeam from './AboutMeetOurTeam'
-
+import React from "react";
+import DefaultBanner from "../../components/DefaultBanner";
+import AboutusLinkPro from "./AboutusLinkPro";
+import AboutusWhyChoose from "./AboutusWhyChoose";
+import AboutUsWhatWeDo from "./AboutUsWhatWeDo";
+import AboutOurValues from "./AboutOurValues";
+import AboutTestimonials from "./AboutTestimonials";
+// import AboutMeetOurTeam from './AboutMeetOurTeam'
 
 function AboutUs() {
   return (
     <>
-    <DefaultBanner componentName="About Us"/>
-    <AboutusLinkPro/>
-    <AboutusWhyChoose/>
-    <AboutUsWhatWeDo/>
-    <AboutOurValues/>
-    <AboutTestimonials/>
-    {/* <AboutMeetOurTeam/> */}
-    
+      <DefaultBanner componentName="About Us" />
+      <AboutusLinkPro />
+      <AboutusWhyChoose />
+      <AboutUsWhatWeDo />
+      <AboutOurValues />
+      <AboutTestimonials />
+      {/* <AboutMeetOurTeam/> */}
     </>
-  )
+  );
 }
 
-export default AboutUs
+export default AboutUs;
